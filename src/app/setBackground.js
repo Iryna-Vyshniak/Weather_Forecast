0d942307d6d1a5b8258969988080a98f50e6c0d8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-03-04 16:46:18 +0200

    add function to set unique background photos that change with the location, use pexels

---
+++ src/app/setBackground.js
@@ -0,0 +1,43 @@
+import { createClient } from 'pexels';
+
+export function setBackground(query) {
+  const client = createClient(
+    '563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74'
+  );
+
+  const defaultImg =
+    'https://dreamdim.ua/wp-content/uploads/2022/03/rus_nax-1024x589.jpeg';
+
+  const randomNumber = max => {
+    return Math.floor(Math.random() * max);
+  };
+
+  client.photos
+    .search({
+      query,
+      per_page: 40,
+      size: 'large',
+    })
+    .then(data => {
+      const index = randomNumber(data.photos.length);
+      console.log('data', data);
+      const { large2x: src = defaultImg } = data.photos[index]?.src ?? {};
+
+      const today = new Date();
+      const dayHour = today.getHours();
+
+      if (dayHour >= 11 && dayHour <= 17) {
+        document.body.style = `background: linear-gradient(rgba(0, 0, 0, 0),  rgba(255, 247, 3, 0.366)),
+  url('${src}') center fixed; background-size: cover;`;
+      } else if (dayHour >= 5 && dayHour <= 11) {
+        document.body.style = `background: linear-gradient(rgba(0, 0, 0, 0), rgba(2, 107, 220, 0.647)),
+  url('${src}') center fixed; background-size: cover;`;
+      } else if (dayHour >= 18 && dayHour <= 23) {
+        document.body.style = `background: linear-gradient(rgba(0, 0, 0, 0), rgba(104, 24, 2, 0.514)),
+  url('${src}') center fixed; background-size: cover;`;
+      } else {
+        document.body.style = `background: linear-gradient(rgba(0, 0, 0, 0), rgba(32, 1, 85, 0.576)),
+  url('${src}') center fixed; background-size: cover;`;
+      }
+    });
+}
